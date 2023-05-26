067fc9c0f58bf21c14a614a2f78d4d90b66b8f5a
Juan Felipe Delgadillo V <89826767+pipejfdv@users.noreply.github.com>
2023-05-25 19:14:11 -0500

    conexion de datos
    se completa el crud

--- formConect.php
+++ formConect.php
@@ -4,37 +4,37 @@
         if(!$enlace){
             die("Conexion no exitosa en base de datos". mysqli_error());
         }
-        echo "conxion exitosa";
         
 // función del boton de registro
 
-if(isset($_POST['registrarse'])){
+
+if(isset($_GET['registrarse'])){
     // crear variables que ingresan a la base de datos
-    echo "ejecutando codigo";
-    var_dump($name);
-    $name = $_POST['nombre'];
-    $lastname = $_POST['apellido'];
-    $date = $_POST['fecha'];
+    
+    
+    $name = $_GET['nombre'];
+    $lastname = $_GET['apellido'];
+    $date = $_GET['fecha'];
     $restartDate = date('Y-m-d', strtotime($date));
-    $typeSex = $_POST['genero'];
-    $email = $_POST['email'];
-    $passw = $_POST['Contrasena'];
-    $rol = $_POST['rol'];
+    $typeSex = $_GET['genero'];
+    $email = $_GET['email'];
+    $passw = $_GET['Contrasena'];
+    $rol = $_GET['rol'];
 
     // consulta SQL para inseción de dato
 
-    $insertDB = "INSERT INTO usuarios () 
+    $insertDB = "INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, genero, correo, contrasena, rol) 
     values ('$name','$lastname', '$restartDate','$typeSex', '$email','$passw', '$rol')";
 
     // envio de datos
     $insertar = mysqli_query($enlace, $insertDB);
-
+    // var_dump($insertDB);
     // comprobar
 
     if(!$insertar){
         echo "Error en la linea: ".mysqli_error($enlace);;
     }
-    var_dump($name);
+    
 }
 
 mysqli_close($enlace);
