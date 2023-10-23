56b1d3ca65c857a952123f57415f3bc314db1219
AndresFelipe21 <111938951+AndresFelipe21@users.noreply.github.com>
2023-10-23 18:45:48 -0500

    Update animaciones.js
    Update animaciones.js

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,7 +1,7 @@
 //Solo animaciones de la pagina
 //se parar las animaciónes por -- 
 
-alert("hola");
+
 //---------Animaciones pagina de registro--------//
 function Login(){
     x.style.left="50px";
@@ -17,19 +17,4 @@ function Registrarme(){
 var x = document.getElementById('Login');
 var y = document.getElementById('Registrarme');
 var z = document.getElementById('elegir');
-
-function elemntoDinamico(url, elemento) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url, false);
-    request.send(null);
-    elemento.innerHTML = request.responseText;
-}
-
-function IngresosUsuario() {
-    elementoDinamico("igresoUsuario.jsp", document.getElementById("control1"));
-}
-
-function LoginUsuario(){
-    elementoDinamico("login.jsp", document.getElementById("control2"));
-}
 
