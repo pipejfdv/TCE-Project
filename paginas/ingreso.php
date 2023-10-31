501f890e0eb69c2b5ee44b43425664e6ad1a7536
AndresFelipe21 <111938951+AndresFelipe21@users.noreply.github.com>
2023-10-31 18:06:36 -0500

    ok
    ok

--- paginas/ingreso.php
+++ paginas/ingreso.php
@@ -60,6 +60,13 @@
       </li>
     </ul>      
   </div>
+  <section>
+    <div class="position-relative">
+      <h1 class= "text-center mt-3">Bienvenido</h1>
+      <h2 class= "text-center my-3">Categoria de juegos</h2>
+    </div>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
